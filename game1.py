5aa4e3e63a6a649cc3b75e3217f6dc44c716dab2
Harkirat Chadha <harkiratchadha@Harkirats-MacBook-Air.local>
2025-08-22 21:40:07 +0530

    Initial commit: setup Snake Game using Pygame

---
+++ game1.py
@@ -0,0 +1,16 @@
+# Snake Game using pygame
+# by @hchadha28
+# Steps involved : 
+# 1. Installing Pygame
+# 2. Create a blank screen
+# 3. Create the food
+# 4. Create the snake
+# 5. Move the snake
+# 6. Make the snake eat the food
+# 7. Make the snake grow
+# 8. Checking for collisions
+# 9. Adding title & frame
+# 10. Adding score
+# 11. Adding sounds
+
+ 
